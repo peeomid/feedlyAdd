bba9812948a1922c3a93b12ac1425542bb64cf0d
Luan Nguyen <lihavim@gmail.com>
2014-06-17 15:28:20 +0700

    Get list of feeds

--- src/js/background.js
+++ src/js/background.js
@@ -1,30 +1,56 @@
-var MAJOR_VERSION = "0.4.2";
-if(localStorage.getItem('version') === null ){
-	chrome.tabs.create({'url': chrome.extension.getURL('options/thankyou.html')}, function(tab){});
-	localStorage.setItem('version',MAJOR_VERSION);
-}else{
-	if (localStorage.getItem('version') != MAJOR_VERSION){
-		localStorage.setItem('version',MAJOR_VERSION);
-	}
-}
-var FEED_HREF = '';
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-	if (changeInfo.status == "complete") {
-		chrome.tabs.executeScript(tab.id, {file: "js/bookmarklet.js"});     
-	 	chrome.pageAction.hide(tabId);   	 
-	 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		  chrome.tabs.sendMessage(tabs[0].id, {greeting: "RSS"}, function(response) {
-		    if(response && response.ResRSS == "true"){
-				chrome.pageAction.show(tabId);  
-				FEED_HREF = 'http://www.feedly.com/home#subscription/feed/'+ response.href;
-				chrome.pageAction.onClicked.addListener(function(tab){      
-					      
-				  chrome.tabs.sendMessage(tabs[0].id, {FEED_HREF:FEED_HREF}, function(response) {
-				  });
-			      
-			    });
-		     }
-		  });
-		}); 
-   } 
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.extension.onMessage.addListener(function(request, sender) {
+  if (request.msg == "feedIcon") {
+    // First validate that all the URLs have the right schema.
+    var input = [];
+    for (var i = 0; i < request.feeds.length; ++i) {
+      var a = document.createElement('a');
+      a.href = request.feeds[i].href;
+      if (a.protocol == "http:" || a.protocol == "https:") {
+        input.push(request.feeds[i]);
+      } else {
+        console.log('Warning: feed source rejected (wrong protocol): ' +
+                    request.feeds[i].href);
+      }
+    }
+
+    if (input.length == 0)
+      return;  // We've rejected all the input, so abort.
+
+    // We have received a list of feed urls found on the page.
+    var feeds = {};
+    feeds[sender.tab.id] = input;
+    chrome.storage.local.set(feeds, function() {
+      // Enable the page action icon.
+      chrome.pageAction.setTitle(
+        { tabId: sender.tab.id,
+          title: "Add feed to Feedly"
+        });
+      chrome.pageAction.show(sender.tab.id);
+    });
+  } //else if (request.msg == "feedDocument") {
+    // var something = "something";
+    // We received word from the content script that this document
+    // is an RSS feed (not just a document linking to the feed).
+    // So, we go straight to the subscribe page in a new tab and
+    // navigate back on the current page (to get out of the xml page).
+    // We don't want to navigate in-place because trying to go back
+    // from the subscribe page takes us back to the xml page, which
+    // will redirect to the subscribe page again (we don't support a
+    // location.replace equivalant in the Tab navigation system).
+    // chrome.tabs.executeScript(sender.tab.id,
+    //     { code: "if (history.length > 1) " +
+    //              "history.go(-1); else window.close();"
+    //     });
+    // var url = "subscribe.html?" + encodeURIComponent(request.href);
+    // url = chrome.extension.getURL(url);
+    // chrome.tabs.create({ url: url, index: sender.tab.index });
+  // }
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId) {
+  chrome.storage.local.remove(tabId.toString());
 });
